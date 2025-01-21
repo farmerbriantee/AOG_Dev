21e558214094322e9d52f227d3fe237e2a0e9477
BrianTee <yesgogit@gmail.com>
2025-01-20 20:30:04 -0700

    intial commit

---
+++ SourceCode/AgOne/Source/Forms/FormPGN.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AgOne
+{
+    public partial class FormPGN : Form
+    {
+        public FormPGN()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSerialOK_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}

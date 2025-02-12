b5eea778a7b0cfa9134b1f77741992683bf628cd
BrianTee <yesgogit@gmail.com>
2025-02-12 09:05:20 -0700

    Start double file picker

--- SourceCode/AOG/Forms/Pickers/FormFilePicker.Designer.cs
+++ SourceCode/AOG/Forms/Pickers/FormFilePicker.Designer.cs
@@ -29,56 +29,22 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.lvLines = new System.Windows.Forms.ListView();
-            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.chDistance = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.chArea = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.btnByDistance = new System.Windows.Forms.Button();
             this.btnOpenExistingLv = new System.Windows.Forms.Button();
             this.btnDeleteAB = new System.Windows.Forms.Button();
             this.btnDeleteField = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
+            this.chName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chDistance = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chArea = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lvLines = new System.Windows.Forms.ListView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.chDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // lvLines
-            // 
-            this.lvLines.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.lvLines.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.lvLines.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.chName,
-            this.chDistance,
-            this.chArea});
-            this.lvLines.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lvLines.FullRowSelect = true;
-            this.lvLines.GridLines = true;
-            this.lvLines.HideSelection = false;
-            this.lvLines.ImeMode = System.Windows.Forms.ImeMode.Off;
-            this.lvLines.Location = new System.Drawing.Point(5, 12);
-            this.lvLines.MultiSelect = false;
-            this.lvLines.Name = "lvLines";
-            this.lvLines.Size = new System.Drawing.Size(986, 459);
-            this.lvLines.Sorting = System.Windows.Forms.SortOrder.Ascending;
-            this.lvLines.TabIndex = 86;
-            this.lvLines.UseCompatibleStateImageBehavior = false;
-            this.lvLines.View = System.Windows.Forms.View.Details;
-            // 
-            // chName
-            // 
-            this.chName.Text = "Field";
-            this.chName.Width = 680;
-            // 
-            // chDistance
-            // 
-            this.chDistance.Text = "Distance";
-            this.chDistance.Width = 140;
-            // 
-            // chArea
-            // 
-            this.chArea.Text = "Area";
-            this.chArea.Width = 140;
             // 
             // timer1
             // 
@@ -90,7 +56,7 @@
             this.btnByDistance.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnByDistance.Image = global::AgOpenGPS.Properties.Resources.Sort;
             this.btnByDistance.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnByDistance.Location = new System.Drawing.Point(210, 507);
+            this.btnByDistance.Location = new System.Drawing.Point(204, 301);
             this.btnByDistance.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnByDistance.Name = "btnByDistance";
             this.btnByDistance.Size = new System.Drawing.Size(147, 63);
@@ -107,7 +73,7 @@
             this.btnOpenExistingLv.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnOpenExistingLv.Image = global::AgOpenGPS.Properties.Resources.FileOpen;
             this.btnOpenExistingLv.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnOpenExistingLv.Location = new System.Drawing.Point(697, 507);
+            this.btnOpenExistingLv.Location = new System.Drawing.Point(691, 301);
             this.btnOpenExistingLv.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.btnOpenExistingLv.Name = "btnOpenExistingLv";
             this.btnOpenExistingLv.Size = new System.Drawing.Size(261, 63);
@@ -123,7 +89,7 @@
             this.btnDeleteAB.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.btnDeleteAB.ForeColor = System.Drawing.SystemColors.ControlText;
             this.btnDeleteAB.Image = global::AgOpenGPS.Properties.Resources.Cancel64;
-            this.btnDeleteAB.Location = new System.Drawing.Point(502, 507);
+            this.btnDeleteAB.Location = new System.Drawing.Point(494, 634);
             this.btnDeleteAB.Name = "btnDeleteAB";
             this.btnDeleteAB.Size = new System.Drawing.Size(71, 63);
             this.btnDeleteAB.TabIndex = 91;
@@ -135,48 +101,145 @@
             this.btnDeleteField.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.btnDeleteField.ForeColor = System.Drawing.SystemColors.ControlText;
             this.btnDeleteField.Image = global::AgOpenGPS.Properties.Resources.Trash;
-            this.btnDeleteField.Location = new System.Drawing.Point(46, 507);
+            this.btnDeleteField.Location = new System.Drawing.Point(40, 301);
             this.btnDeleteField.Name = "btnDeleteField";
             this.btnDeleteField.Size = new System.Drawing.Size(71, 63);
             this.btnDeleteField.TabIndex = 94;
             this.btnDeleteField.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             this.btnDeleteField.Click += new System.EventHandler(this.btnDeleteField_Click);
             // 
-            // label1
+            // chName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(43, 488);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 16);
-            this.label1.TabIndex = 95;
-            this.label1.Text = "Delete Field";
+            this.chName.Text = "Field";
+            this.chName.Width = 680;
             // 
-            // label2
+            // chDistance
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(515, 488);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 16);
-            this.label2.TabIndex = 96;
-            this.label2.Text = "Cancel";
+            this.chDistance.Text = "Distance";
+            this.chDistance.Width = 140;
+            // 
+            // chArea
+            // 
+            this.chArea.Text = "Area";
+            this.chArea.Width = 140;
+            // 
+            // lvLines
+            // 
+            this.lvLines.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvLines.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lvLines.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chName,
+            this.chDistance,
+            this.chArea});
+            this.lvLines.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvLines.FullRowSelect = true;
+            this.lvLines.GridLines = true;
+            this.lvLines.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.lvLines.HideSelection = false;
+            this.lvLines.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.lvLines.Location = new System.Drawing.Point(5, 6);
+            this.lvLines.MultiSelect = false;
+            this.lvLines.Name = "lvLines";
+            this.lvLines.Size = new System.Drawing.Size(981, 279);
+            this.lvLines.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.lvLines.TabIndex = 86;
+            this.lvLines.UseCompatibleStateImageBehavior = false;
+            this.lvLines.View = System.Windows.Forms.View.Details;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chDate,
+            this.columnHeader1});
+            this.listView1.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listView1.HideSelection = false;
+            this.listView1.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.listView1.Location = new System.Drawing.Point(5, 380);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(981, 240);
+            this.listView1.Sorting = System.Windows.Forms.SortOrder.Ascending;
+            this.listView1.TabIndex = 97;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // chDate
+            // 
+            this.chDate.Text = "Date Created";
+            this.chDate.Width = 400;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Job";
+            this.columnHeader1.Width = 575;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Image = global::AgOpenGPS.Properties.Resources.Trash;
+            this.button1.Location = new System.Drawing.Point(25, 634);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 63);
+            this.button1.TabIndex = 101;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = global::AgOpenGPS.Properties.Resources.Sort;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(189, 634);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 63);
+            this.button2.TabIndex = 100;
+            this.button2.Text = "Sort";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Yes;
+            this.button3.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = global::AgOpenGPS.Properties.Resources.FileOpen;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(691, 634);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(261, 63);
+            this.button3.TabIndex = 99;
+            this.button3.Text = "Use Selected";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // FormFilePicker
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.WhiteSmoke;
-            this.ClientSize = new System.Drawing.Size(991, 578);
+            this.ClientSize = new System.Drawing.Size(991, 702);
             this.ControlBox = false;
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
             this.Controls.Add(this.btnDeleteField);
             this.Controls.Add(this.btnByDistance);
             this.Controls.Add(this.btnOpenExistingLv);
             this.Controls.Add(this.btnDeleteAB);
             this.Controls.Add(this.lvLines);
             this.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(5);
             this.Name = "FormFilePicker";
             this.ShowInTaskbar = false;
@@ -184,22 +247,24 @@
             this.Text = "FormFilePicker";
             this.Load += new System.EventHandler(this.FormFilePicker_Load);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListView lvLines;
-        private System.Windows.Forms.ColumnHeader chName;
-        private System.Windows.Forms.ColumnHeader chDistance;
         private System.Windows.Forms.Button btnByDistance;
         private System.Windows.Forms.Button btnOpenExistingLv;
         private System.Windows.Forms.Button btnDeleteAB;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.ColumnHeader chArea;
         private System.Windows.Forms.Button btnDeleteField;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader chName;
+        private System.Windows.Forms.ColumnHeader chDistance;
+        private System.Windows.Forms.ColumnHeader chArea;
+        private System.Windows.Forms.ListView lvLines;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader chDate;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
